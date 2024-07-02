4385532f5ce2352858bb4d86d8dba767623828d9
Huu Phuc Tran <kentr9989@gmail.com>
2024-07-03 00:33:57 +1000

    Reimplement Newsletter with Mantine component

--- client/src/components/newsletter/Newsletter.jsx
+++ client/src/components/newsletter/Newsletter.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
-import classes from './newsletter.module.css'
-import { FiSend } from "react-icons/fi";
+import React from 'react';
+import { Box, Container, Stack, Title, Text, Group, TextInput, ActionIcon } from '@mantine/core';
+import { FiSend } from 'react-icons/fi';
+import classes from './newsletter.module.css';
 
 const Newsletter = () => {
   return (
-    <div className={classes.container}>
-      <div className={classes.wrapper}>
-        <div className={classes.titles}>
-          <h5> Want to get latest update of the market ?</h5>
-          <h2> Send us your email and we will contact you</h2>
-        </div>
-        <div className={classes.inputContainer}>
-          <input type="email" placeholder='Type your email' />
-          <FiSend className={classes.sendIcon}/>
-        </div>
-      </div>
-    </div>
-  )
-}
+    <Box className={classes.container}>
+      <Container size="lg" className={classes.wrapper}>
+        <Stack align="center" className={classes.titles}>
+          <Text component="h5" className={classes.subtitle}>
+            Want to get the latest updates on the market?
+          </Text>
+          <Title order={2} className={classes.title}>
+            Send us your email and we will contact you
+          </Title>
+        </Stack>
+        <Group className={classes.inputContainer}>
+          <TextInput
+            placeholder="Type your email"
+            className={classes.input}
+            variant="unstyled"
+          />
+          <ActionIcon size="lg" variant="filled" color="blue" className={classes.sendIcon}>
+            <FiSend />
+          </ActionIcon>
+        </Group>
+      </Container>
+    </Box>
+  );
+};
 
-export default Newsletter
+export default Newsletter;
